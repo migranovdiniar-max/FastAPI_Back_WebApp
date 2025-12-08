d149cc061de4764bc6c8eb905bfa52e751ccbfc5
Dinyar <migranovdiniar@gmail.com>
2025-12-08 19:39:23 +0300

    added potry.toml, lock

--- main.py
+++ main.py
@@ -1,11 +1,36 @@
 from fastapi import FastAPI
+from pydantic import EmailStr, BaseModel
 
 app = FastAPI()
+
+
+class CreateUser(BaseModel):
+    email: EmailStr
 
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
+
+
+@app.get("/hello/")
+def read_hello(name: str = "World"):
+    if name:
+        return {
+            "Hello": name,
+        }
+    
+
+@app.post("/users/")
+def create_user(user: CreateUser):
+    return user
+
+
+@app.post("/calc/add/")
+def calc_add(a: int, b: int):
+    return {
+        "result": a + b 
+    }
 
 
 @app.get("/items/")
@@ -14,3 +39,18 @@ def list_items():
         {"id": 1, "name": "Item 1"},
         {"id": 2, "name": "Item 2"},
     ]
+
+
+@app.get("/item/latest/")
+def get_latest_item():
+    return {
+        "id": 2,
+        "name": "Item 2",
+    }
+
+
+@app.get("/items/{item_id}")
+def get_item(item_id: int):
+    return {
+        "id": item_id,
+    }
