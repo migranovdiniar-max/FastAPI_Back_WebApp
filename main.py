ed978bd7aa8a238df66052443ea9d270dc825669
Dinyar <migranovdiniar@gmail.com>
2025-12-08 19:00:06 +0300

    started an app

--- main.py
+++ main.py
@@ -6,3 +6,11 @@ app = FastAPI()
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
+
+
+@app.get("/items/")
+def list_items():
+    return [
+        {"id": 1, "name": "Item 1"},
+        {"id": 2, "name": "Item 2"},
+    ]
