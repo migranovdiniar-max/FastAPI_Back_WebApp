76aab5c701e64afcd9f81a9d91b1ff37822267bc
Dinyar <migranovdiniar@gmail.com>
2025-12-08 20:30:39 +0300

    added crud, schemas, view for users

--- items_views.py
+++ items_views.py
@@ -4,6 +4,7 @@ from typing import Annotated
 
 router = APIRouter(
     prefix="/items",
+    tags=["items"],
 )
 
 @router.get("/")
--- main.py
+++ main.py
@@ -2,36 +2,9 @@ from fastapi import FastAPI, Path
 from typing import Annotated
 from pydantic import EmailStr, BaseModel
 from items_views import router as items_router
+from users.views import router as users_router
 
 
 app = FastAPI()
 app.include_router(items_router)
-
-
-class CreateUser(BaseModel):
-    email: EmailStr
-
-
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
-
-
-@app.get("/hello/")
-def read_hello(name: str = "World"):
-    if name:
-        return {
-            "Hello": name,
-        }
-    
-
-@app.post("/users/")
-def create_user(user: CreateUser):
-    return user
-
-
-@app.post("/calc/add/")
-def calc_add(a: int, b: int):
-    return {
-        "result": a + b 
-    }
+app.include_router(users_router)
